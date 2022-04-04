0235e52978c48c8dd4f2a298ec63d2f4ba1cc7f2
MateusFS99 <mateusfsantos.pfs@gmail.com>
2022-04-04 13:03:31 -0300

    - Controllers 100%

--- Controllers/IngredienteController.cs
+++ Controllers/IngredienteController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using RecipeBook_ASP.NET.Data;
+using RecipeBook_ASP.NET.Models;
 
 namespace RecipeBook_ASP.NET.Controllers
 {
@@ -42,6 +43,65 @@ namespace RecipeBook_ASP.NET.Controllers
             {
                 return BadRequest($"Erro: {ex.Message}");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Ingrediente ingrediente)
+        {
+            try
+            {
+                _repo.Add(ingrediente);
+                if (await _repo.SaveChangesAsync())
+                    return Ok(ingrediente);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{ingredienteId}")]
+        public async Task<IActionResult> Put(Ingrediente ingrediente, int ingredienteId)
+        {
+            try
+            {
+                bool exists = await _repo.GetIngredienteAsyncById(ingredienteId) != null;
+
+                if (!exists)
+                    return NotFound();
+                _repo.Update(ingrediente);
+                if (await _repo.SaveChangesAsync())
+                    return Ok(ingrediente);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{ingredienteId}")]
+        public async Task<IActionResult> Delete(int ingredienteId)
+        {
+            try
+            {
+                var ingrediente = await _repo.GetIngredienteAsyncById(ingredienteId);
+
+                if (ingrediente == null)
+                    return NotFound();
+                _repo.Delete(ingrediente);
+                if (await _repo.SaveChangesAsync())
+                    return Ok("Deleted!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
         }
     }
 }
--- Controllers/ReceitaController.cs
+++ Controllers/ReceitaController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using RecipeBook_ASP.NET.Data;
+using RecipeBook_ASP.NET.Models;
 
 namespace RecipeBook_ASP.NET.Controllers
 {
@@ -43,6 +44,65 @@ namespace RecipeBook_ASP.NET.Controllers
             {
                 return BadRequest($"Erro: {ex.Message}");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Receita receita)
+        {
+            try
+            {
+                _repo.Add(receita);
+                if (await _repo.SaveChangesAsync())
+                    return Ok(receita);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{receitaId}")]
+        public async Task<IActionResult> Put(Receita receita, int receitaId)
+        {
+            try
+            {
+                bool exists = await _repo.GetReceitaAsyncById(receitaId, false) != null;
+
+                if (!exists)
+                    return NotFound();
+                _repo.Update(receita);
+                if (await _repo.SaveChangesAsync())
+                    return Ok(receita);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{receitaId}")]
+        public async Task<IActionResult> Delete(int receitaId)
+        {
+            try
+            {
+                var receita = await _repo.GetReceitaAsyncById(receitaId, false);
+
+                if (receita == null)
+                    return NotFound();
+                _repo.Delete(receita);
+                if (await _repo.SaveChangesAsync())
+                    return Ok("Deleted!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
         }
     }
 }
